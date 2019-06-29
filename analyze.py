bd04fc23cc301df01c281b8423890363dc236605
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2019-06-30 02:45:15 +0900

    :+1: Implement single_source_shortest_path from Atlanta

--- analyze.py
+++ analyze.py
@@ -1,7 +1,18 @@
+import matplotlib.pyplot as plt
 import networkx as nx
+import pandas as pd
+from pprint import pprint
 
 def main():
-    pass
+    Graph = nx.Graph()
+    cities = pd.read_csv('data/city.csv')
+    for idx, row in cities.iterrows():
+        Graph.add_node(row['Id'], name=row['Label'], color=row['Color'])
+    edges = pd.read_csv('data/edge.csv')
+    for idx, row in edges.iterrows():
+        Graph.add_edge(row['Source'], row['Target'])
+    pprint(nx.single_source_shortest_path(Graph, source=0))
+    nx.nx_agraph.view_pygraphviz(Graph, prog='fdp')
 
 
 if __name__ == "__main__":
