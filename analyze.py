0cde8f14f3dd7d52010ad5140db8f36817d49f59
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2019-07-01 02:07:03 +0900

    :+1: Add pandemic_rules.pdf

--- analyze.py
+++ analyze.py
@@ -1,18 +1,28 @@
-import matplotlib.pyplot as plt
-import networkx as nx
-import pandas as pd
 from pprint import pprint
 
+import networkx as nx
+import pandas as pd
+
+
 def main():
-    Graph = nx.Graph()
+    G = nx.Graph()
     cities = pd.read_csv('data/city.csv')
     for idx, row in cities.iterrows():
-        Graph.add_node(row['Id'], name=row['Label'], color=row['Color'])
+        gviz_attr = {
+            'color': row['Color'],
+            'fillcolor': row['Color'],
+            'fontcolor': 'grey',
+            'fontname': 'monospaced',
+            'shape': 'hexagon',
+            'style': 'filled',
+            'width': 1.5, 'height': 1.5
+        }
+        G.add_node(row['Name'], **gviz_attr)
     edges = pd.read_csv('data/edge.csv')
     for idx, row in edges.iterrows():
-        Graph.add_edge(row['Source'], row['Target'])
-    pprint(nx.single_source_shortest_path(Graph, source=0))
-    nx.nx_agraph.view_pygraphviz(Graph, prog='fdp')
+        G.add_edge(row['Source'], row['Target'])
+    pprint(nx.single_source_shortest_path(G, source='Atlanta'))
+    nx.nx_agraph.view_pygraphviz(G, prog='fdp')
 
 
 if __name__ == "__main__":
